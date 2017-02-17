5b12a1c3b46ddeca51ed53d7ff6ed8183bd0f037
Ian Vaughan <github@ianvaughan.co.uk>
2017-02-17 22:43:36 +0000

    mix new spot

---
+++ spot/lib/spot.ex
@@ -0,0 +1,18 @@
+defmodule Spot do
+  @moduledoc """
+  Documentation for Spot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Spot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ spot/test/spot_test.exs
@@ -0,0 +1,8 @@
+defmodule SpotTest do
+  use ExUnit.Case
+  doctest Spot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
